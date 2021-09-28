5f512f443bc7ec7874cfe725fac5db00bd51de1e
Wesly van Zoen <wvzoen@hotmail.com>
2021-09-28 09:16:23 +0200

    .map excercises

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Title from "./components/Title";
+import Pokemon from "./components/Pokemon";
 
 function App() {
+  const all_pokemon = [
+    { name: "Charizard", weight: 90 },
+    { name: "Bulbasaur", weight: 6.9 },
+    { name: "Mewtwo", weight: 122 },
+    { name: "Mega beedrill", weight: 65 },
+  ];
+
+  all_pokemon.map((p) => console.log(p.weight));
+  all_pokemon.map((p) => console.log(p.name + ": " + p.weight + " kg"));
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Welcome to Codaisseur
-        </a>
-      </header>
+      <main>
+        <Title content="Some Simple Title" />
+        <Pokemon
+          name="naam"
+          weight={1}
+          awesome={true}
+          terrifying={false}
+          abilities={["ab1", "ab2", "ab3"]}
+        />
+        <Pokemon
+          name="naam2"
+          weight={1.1}
+          awesome={true}
+          terrifying={false}
+          abilities={["ab11", "ab21", "ab31"]}
+        />
+      </main>
     </div>
   );
 }
